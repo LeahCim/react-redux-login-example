2c257716455e867d873ab22b7ee8c1d666137c01
Michael <bogus739-github@yahoo.de>
2018-08-08 14:43:41 +0100

    Add original Cypress tests + eslint-plugin-import

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,42 @@
+describe('/login', () => {
+    beforeEach(() => {
+        cy.visit('/login');
+    });
+
+    it('Successfully logs in', () => {
+        cy.contains('Log out')
+            .should('not.exist');
+
+        cy.contains('button', 'Log in')
+            .click();
+
+        cy.contains('Log out')
+
+        cy.url()
+            .should('include', '/data');
+    });
+
+    ['username', 'password'].map(field => {
+        it(`Disables the button when ${field} empty`, () => {
+            cy.get(`#${field}`).clear();
+
+            cy.get('[type=submit]')
+                .should('be.disabled');
+        });
+    });
+
+    it("Lands back on login page after wrong credentials submitted", () => {
+        cy.get('#username').type("Wrong");
+
+        cy.get('[type=submit')
+    });
+
+    it("Stays logged in after page refresh", () => {
+        cy.contains('button', 'Log in')
+            .click();
+
+        cy.reload();
+
+        cy.contains('Log out');
+    });
+});
