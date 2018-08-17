e99512c8efdd1b7c5b79fe9d193ea0ec434c801c
Michael <bogus739-github@yahoo.de>
2018-08-17 17:34:52 +0100

    Only force logout on 401 unauthorised error

--- src/middlewares/deauthoriser.js
+++ src/middlewares/deauthoriser.js
@@ -2,10 +2,14 @@ import { RECEIVE_DATA } from '../actions/actionTypes';
 import { deleteCredentials } from '../actions/credentialsActionCreators';
 
 const deauthoriser = store => next => action => {
-    if (action.type === RECEIVE_DATA && action.error)
+    if (action.type === RECEIVE_DATA &&
+        action.error &&
+        action.payload.status === 401) {
+
         store.dispatch(deleteCredentials());
+    }
 
     return next(action);
 };
 
-export default deauthoriser;
+export default deauthoriser
