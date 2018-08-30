a2cc69d26ee13566a79323f81ad6219d8659169d
Michael <bogus739-github@yahoo.de>
2018-08-30 12:51:53 +0100

    Extract store initialisation into own file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import registerServiceWorker from './registerServiceWorker';
 import AppContainer from './components/AppContainer';
-import rootReducer from './reducers/rootReducer';
-import middlewares from './middlewares';
-
-import { PENDING_CREDENTIALS } from './constants';
-
-const initialState = { credentials: PENDING_CREDENTIALS };
-const enhancers = composeWithDevTools(middlewares);
-
-const store = createStore(rootReducer, initialState, enhancers);
+import store from './store'
 
 ReactDOM.render(
     <Provider store={store}>
---
+++ src/store.js
@@ -0,0 +1,13 @@
+
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from './reducers/rootReducer';
+import middlewares from './middlewares';
+
+import { PENDING_CREDENTIALS } from './constants';
+
+const initialState = { credentials: PENDING_CREDENTIALS };
+const enhancers = composeWithDevTools(middlewares);
+
+export default createStore(rootReducer, initialState, enhancers);
