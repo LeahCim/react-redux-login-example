a56ccd2e9c6618b73f83cf830d25742eb0aca910
Michael <bogus739-github@yahoo.de>
2018-08-31 10:43:23 +0100

    Move jest-localstorage-mock import to setupTests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import AppContainer from './AppContainer';
-import "jest-localstorage-mock";
 
 const mockStore = configureMockStore();
 
