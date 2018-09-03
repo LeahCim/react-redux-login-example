907d3ae82cdec716d286aab3cb89b75ed18e8a16
Michael <bogus739-github@yahoo.de>
2018-09-03 16:42:01 +0100

    Upgrade Bootstrap v3->v4; improve Login form

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import registerServiceWorker from './registerServiceWorker';
 import AppContainer from './components/AppContainer';
 import store from './store';
