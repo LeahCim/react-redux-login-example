b6c68ebb379b30264a8b5f4d6d6f4c5b9123fb69
Michael <bogus739-github@yahoo.de>
2018-09-04 10:12:36 +0100

    Don't require 'disabled' prop in LoginButton

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Button } from 'reactstrap';
-import PropTypes from 'prop-types';
 
-export const LoginButton = ({ disabled }) =>
+export const LoginButton = ({ ...props }) =>
     <Button
         type="submit"
         size="lg"
         block
-        disabled={disabled}
+        {...props}
     >
         Log in
     </Button>;
-
-LoginButton.propTypes = {
-    disabled: PropTypes.bool.isRequired
-}
