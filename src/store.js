03eef5a7f2178174390c0561c6f211d731de5d70
Michael <bogus739-github@yahoo.de>
2018-09-04 17:52:46 +0100

    Refactor reducers

--- src/store.js
+++ src/store.js
@@ -1,10 +1,9 @@
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import initialState from './initialState';
-import rootReducer from './reducers/rootReducer';
+import rootReducer from './reducers';
 import middlewares from './middlewares';
 
 const enhancers = composeWithDevTools(middlewares);
 
-export default createStore(rootReducer, initialState, enhancers);
+export default createStore(rootReducer, {}, enhancers);
