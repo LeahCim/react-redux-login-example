79990e2c68e04399d5c1d02f5abf1c0609ea8527
Michael <bogus739-github@yahoo.de>
2018-09-04 17:53:58 +0100

    Refactor reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,41 +1,9 @@
-import { RECEIVE_DATA } from '../actions/actionTypes';
-import { PERSISTER_RECEIVE } from '../actions/persisterActionTypes';
-import { CREDENTIALS, NO_CREDENTIALS } from '../constants';
-import initialState from '../initialState';
+import { combineReducers } from 'redux';
 
-function stateFromCredentials(prevState, credentials) {
-    const state = credentials === NO_CREDENTIALS ? initialState : prevState;
+import credentialsReducer from './credentialsReducer';
+import dataReducer from './dataReducer';
 
-    return {
-        ...state,
-        credentials
-    }
-}
-
-function persisterReceive(state, action) {
-    switch (action.key) {
-        case CREDENTIALS: return stateFromCredentials(
-            state,
-            action.value || NO_CREDENTIALS
-        );
-
-        default: return state;
-    }
-}
-
-function receiveData(state, action) {
-    if (action.error) return state;
-
-    return {
-        ...state,
-        data: action.payload.Results
-    };
-}
-
-export default function rootReducer(state = {}, action) {
-    switch (action.type) {
-        case PERSISTER_RECEIVE: return persisterReceive(state, action);
-        case RECEIVE_DATA: return receiveData(state, action);
-        default: return state;
-    }
-}
+export default combineReducers({
+    credentials: credentialsReducer,
+    data: dataReducer
+});
