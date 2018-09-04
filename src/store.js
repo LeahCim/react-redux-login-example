0e1ab4f52f2cfa79b441bc4386026e2e45175237
Michael <bogus739-github@yahoo.de>
2018-09-04 17:52:46 +0100

    Refactor reducers

--- src/store.js
+++ src/store.js
@@ -2,15 +2,14 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 
-import initialState from './initialState';
-import rootReducer from './reducers/rootReducer';
 import rootSaga from './sagas';
+import rootReducer from './reducers';
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = applyMiddleware(sagaMiddleware);
 const enhancers = composeWithDevTools(middlewares);
 
-const store = createStore(rootReducer, initialState, enhancers);
+const store = createStore(rootReducer, {}, enhancers);
 sagaMiddleware.run(rootSaga);
 
 export default store;
