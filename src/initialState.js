6e161f16b338a8ff1e4e40c9d4b70f7e32414724
Michael <bogus739-github@yahoo.de>
2018-09-04 16:16:04 +0100

    Reset state after log out

---
+++ src/initialState.js
@@ -0,0 +1,8 @@
+import { PENDING_CREDENTIALS } from './constants';
+
+const initialState = {
+    credentials: PENDING_CREDENTIALS,
+    data: []
+};
+
+export default initialState;
--- src/store.js
+++ src/store.js
@@ -1,12 +1,10 @@
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+import initialState from './initialState';
 import rootReducer from './reducers/rootReducer';
 import middlewares from './middlewares';
 
-import { PENDING_CREDENTIALS } from './constants';
-
-const initialState = { credentials: PENDING_CREDENTIALS };
 const enhancers = composeWithDevTools(middlewares);
 
 export default createStore(rootReducer, initialState, enhancers);
