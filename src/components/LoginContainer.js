b2f255c6bdbe9fb42daf7c17bb6bb91bdaa58f9f
Michael <bogus739-github@yahoo.de>
2018-09-04 12:29:45 +0100

    Move remaining Login logic into LoginContainer

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -1,11 +1,51 @@
-import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
 
 import Login from './Login';
 import { saveCredentials } from '../actions/credentialsActionCreators';
+import { DEFAULT_USERNAME, DEFAULT_PASSWORD } from '../config';
+import { DATA } from './shared/routes';
 
-const mapDispatchToProps = {
-    saveCredentials
-};
+class LoginContainer extends Component {
+    static propTypes = {
+        history: PropTypes.object.isRequired,
+        dispatch: PropTypes.func.isRequired
+    }
 
-export default connect(null, mapDispatchToProps)(withRouter(Login));
+    state = {
+        username: DEFAULT_USERNAME,
+        password: DEFAULT_PASSWORD
+    }
+
+    onChangeOf = key => ({ target }) =>
+        this.setState({
+            [key]: target.value
+        })
+
+    onSubmit = (event) => {
+        event.preventDefault();
+
+        const { username, password } = this.state;
+        const { dispatch, history } = this.props;
+
+        dispatch(saveCredentials(username, password));
+        history.push(DATA);
+    }
+
+    isFormValid = () =>
+        this.state.username.length && this.state.password.length
+
+    render = () =>
+        <Login
+            username={this.state.username}
+            password={this.state.password}
+            onSubmit={this.onSubmit}
+            onUsernameChange={this.onChangeOf('username')}
+            onPasswordChange={this.onChangeOf('password')}
+            isFormValid={this.isFormValid}
+        />;
+}
+
+export default connect()(withRouter(LoginContainer));
